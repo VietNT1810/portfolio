6576cbaf6c64c09c0935a301774f020cb87218a9
Nguyen Tuan Viet <tuanviet19xx@gmail.com>
2026-08-19 16:18:12 +0700

    Update add dot grid background

--- app/app.vue
+++ app/app.vue
@@ -2,9 +2,21 @@
   <div
     class="mx-auto w-full max-w-300 border-x border-b border-border bg-foreground"
   >
+    <Teleport to="body">
+      <UiDotGridSpotlight
+        :spacing="16"
+        :interaction-radius="160"
+        :dotColor="isDark ? 'rgba(255,255,255,0.05)' : 'rgba(0,0,0,0.05)'"
+        :activeDotColor="isDark ? 'rgba(255,255,255,0.1)' : 'rgba(0,0,0,0.1)'"
+        class="fixed! z-[-1]"
+      />
+    </Teleport>
     <Header />
     <NuxtPage />
     <Footer />
     <UiScrollTop />
   </div>
 </template>
+<script setup lang="ts">
+const isDark = useDark();
+</script>
